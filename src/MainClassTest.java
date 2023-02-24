212a276afa4f8768c3af73b1409694f7210d7669
Platonov Anton <anton.pl@hectasoft.ru>
2023-02-24 19:56:00 +0300

    test: First lesson, second test.

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -3,8 +3,15 @@ import org.junit.Test;
 
 public class MainClassTest {
 
+    MainClass mainClass = new MainClass();
+
     @Test
     public void testGetLocalNumber(){
-        Assert.assertTrue("The method getLocalNumber() doesn't return 14!", new MainClass().getLocalNumber() == 14);
+        Assert.assertTrue("The method getLocalNumber() doesn't return 14!", mainClass.getLocalNumber() == 14);
+    }
+
+    @Test
+    public void testGetClassNumber(){
+        Assert.assertTrue("The method getClassNumber() returns a number that is less than or equal to 45.", mainClass.getClassNumber() > 45);
     }
 }
