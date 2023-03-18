6dfc8880bcf77dd73a33cfc486124cc8d3b88b8e
Platonov Anton <anton.pl@hectasoft.ru>
2023-03-18 15:24:51 +0300

    test: Fourth lesson, first test.

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,10 +1,13 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -124,6 +127,139 @@ public class FirstTest {
 
     }
 
+    @Test
+    public void testSaveArticlesToMyList() {
+        String saveButtonLocator = "//android.widget.TextView[@content-desc='Save']";
+        String searchEditLocator = "//*[contains(@resource-id,'org.wikipedia:id/search_src_text')]";
+        String navigationUpButtonLocator = "//android.widget.ImageButton[@content-desc='Navigate up']";
+        String clearQueryButtonLocator = "//android.widget.ImageView[@content-desc='Clear query']";
+
+        //find and save first article
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cann't find field for input search text",
+                5
+        );
+        waitForElementAndSendKey(
+                By.xpath(searchEditLocator),
+                "Java",
+                "Cann't find field for input search text",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_results_list']//android.widget.TextView[contains(@text,'Object-oriented programming language') and contains(@resource-id,'org.wikipedia:id/page_list_item_description')]"),
+                "Cann't find field for input search text",
+                5
+        );
+        waitForElementPresent(
+                By.xpath("//*[@content-desc='Java (programming language)']"),
+                "Cannot find article title",
+                10
+        );
+
+        waitForElementAndClick(
+                By.xpath(saveButtonLocator),
+                "Cannot find 'Save' button",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath(navigationUpButtonLocator),
+                "Cannot find navigate button",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath(clearQueryButtonLocator),
+                "Cannot find options cancel search button",
+                10
+        );
+
+        //find and save second article
+        waitForElementAndSendKey(
+                By.xpath(searchEditLocator),
+                "Appium",
+                "Cann't find field for input search text",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_results_list']//android.widget.TextView[contains(@text,'Automation for Apps') and contains(@resource-id,'org.wikipedia:id/page_list_item_description')]"),
+                "Cann't find field for input search text",
+                5
+        );
+        waitForElementPresent(
+                By.xpath("//*[@content-desc='Automation for Apps']"),
+                "Cannot find article title",
+                10
+        );
+
+        waitForElementAndClick(
+                By.xpath(saveButtonLocator),
+                "Cannot find 'Save' button",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath(navigationUpButtonLocator),
+                "Cannot find navigate button",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_toolbar']/android.widget.ImageButton"),
+                "Cannot find back button",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/nav_tab_reading_lists']"),
+                "Cannot find button of bottom menu 'Saved'",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/recycler_view']"),
+                "Cannot find list of saved articles 'Saved'",
+                5
+        );
+
+        swipeElementToLeft(
+                By.xpath("//android.view.ViewGroup[@resource-id='org.wikipedia:id/reading_list_swipe_refresh']//android.widget.TextView[contains(@text,'Object-oriented programming language') and contains(@resource-id,'org.wikipedia:id/page_list_item_description')]"),
+                "Cannot find item list of saved articles"
+        );
+
+        waitForElementNotPresent(
+                By.xpath("//android.view.ViewGroup[@resource-id='org.wikipedia:id/reading_list_swipe_refresh']//android.widget.TextView[contains(@text,'Object-oriented programming language') and contains(@resource-id,'org.wikipedia:id/page_list_item_description')]"),
+                "Articles by Java exist in list of saved articles",
+                5
+        );
+
+        waitForElementPresent(
+                By.xpath("//android.view.ViewGroup[@resource-id='org.wikipedia:id/reading_list_swipe_refresh']//android.widget.TextView[contains(@text,'Automation for Apps') and contains(@resource-id,'org.wikipedia:id/page_list_item_description')]"),
+                "Cannot find article about Appium in saved list",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.view.ViewGroup[@resource-id='org.wikipedia:id/reading_list_swipe_refresh']//android.widget.TextView[contains(@text,'Automation for Apps') and contains(@resource-id,'org.wikipedia:id/page_list_item_description')]"),
+                "Cannot find article in saved list",
+                5
+        );
+
+        String actual_title = waitForElementAndGetAttribute(
+                By.xpath("//*[@resource-id='pcs-edit-section-title-description']"),
+                "content-desc",
+                "Cannot find title of article about Appium",
+                10
+        );
+
+        Assert.assertEquals(
+                "Title of article not equal expected value 'Automation for Apps'",
+                "Automation for Apps",
+                actual_title
+        );
+    }
+
     /*============================================================================================*/
 
     private WebElement assertElementHasText(By by, String text, String errorMessage){
@@ -168,5 +304,25 @@ public class FirstTest {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSecond);
         wait.withMessage(errorMessage + "\n");
         return wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
+    }
+
+    protected void swipeElementToLeft(By by, String errorMessage) {
+        WebElement element = waitForElementPresent(by, errorMessage, 10);
+        int left_x = element.getLocation().getX();
+        int right_x = left_x + element.getSize().getWidth();
+        int upper_y = element.getLocation().getY();
+        int lower_y = element.getSize().getHeight();
+        int middle_y = upper_y + lower_y/2;
+        TouchAction action = new TouchAction(driver);
+        action.press(right_x,middle_y)
+                .waitAction(300)
+                .moveTo(left_x,middle_y)
+                .release()
+                .perform();
+    }
+
+    private String waitForElementAndGetAttribute(By by, String attribute, String errorMessage, long timeoutInSeconds){
+        WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+        return element.getAttribute(attribute);
     }
 }
